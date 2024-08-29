7dd0afdd5da339c91fb1fa21ac16b551760c634b
Alheli De Vicente <devicentealheli@gmail.com>
2024-08-29 11:37:45 -0600

    mi primer commit

---
+++ src/main/java/com/example/demo/FleetManagmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetManagmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetManagmentApiApplication.class, args);
+	}
+
+}
