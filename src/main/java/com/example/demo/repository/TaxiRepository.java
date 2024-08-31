d1821b1723bea42feeda579a3ea4bd04649ee8ea
Alheli De Vicente <devicentealheli@gmail.com>
2024-08-31 13:14:05 -0600

    slight error lmao

--- src/main/java/com/example/demo/repository/TaxiRepository.java
+++ src/main/java/com/example/demo/repository/TaxiRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaxiRepository  extends JpaRepository <Taxi,Integer>{
+public interface TaxiRepository  extends JpaRepository <Taxi,Integer> {
 }
