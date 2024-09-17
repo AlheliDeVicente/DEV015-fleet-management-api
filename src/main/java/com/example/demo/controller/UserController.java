2d06cb68127abb465a321d138bb70ef0cef44abe
Alheli De Vicente <devicentealheli@gmail.com>
2024-09-17 14:26:38 -0600

    it now responds for a new user!

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+@Autowired
+    private UserService userService;
+
+    @PostMapping("/users")
+    private ResponseEntity<User> createUser (@RequestBody User user){
+        User newUser = userService.saveOrUpdate(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
 }
+
+
